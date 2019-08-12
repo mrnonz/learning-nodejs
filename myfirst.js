a79e5382a10bbf349e8d241e68f9eea2975e1284
Nontawat Numor <nontawat@jibsoft.co.th>
2019-08-12 14:47:28 +0700

    Beautify file :tea:

--- myfirst.js
+++ myfirst.js
@@ -3,7 +3,9 @@ var dt = require('./myfirstmodule');
 var url = require('url');
 
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
     res.write("The data and time are currently: " + dt.myDateTime());
     res.write(req.url);
 
