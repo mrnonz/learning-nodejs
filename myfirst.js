2be28fe6630d0c32745f33595d9a75d030c343aa
Nontawat Numor <nontawat@jibsoft.co.th>
2019-08-12 15:03:04 +0700

    Add Event :tea:

--- myfirst.js
+++ myfirst.js
@@ -1,15 +1,12 @@
 var http = require('http');
-var dt = require('./myfirstmodule');
-var url = require('url');
+var fs = require('fs');
 
 http.createServer(function (req, res) {
-    res.writeHead(200, {
-        'Content-Type': 'text/html'
+    fs.readFile('demofile1.html', function (err, data) {
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+        res.write(data);
+        res.end();
     });
-    res.write("The data and time are currently: " + dt.myDateTime());
-    res.write(req.url);
-
-    var q = url.parse(req.url, true).query;
-    var txt = q.year + " " + q.month;
-    res.end(txt);
 }).listen(8080);
