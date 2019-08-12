5e573308b13778c6fa0d7dc16ed7b05894225304
Nontawat Numor <nontawat@jibsoft.co.th>
2019-08-12 14:41:39 +0700

    Add req.url :tea:

--- myfirst.js
+++ myfirst.js
@@ -4,5 +4,6 @@ var dt = require('./myfirstmodule');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write("The data and time are currently: " + dt.myDateTime());
+    res.write(req.url);
     res.end('Hello world');
 }).listen(8080);
