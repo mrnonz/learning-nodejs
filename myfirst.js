3712c32d4a236c82df6dc2a196076d92569fea85
Nontawat Numor <nontawat@jibsoft.co.th>
2019-08-12 15:08:58 +0700

    Create upload form :tea:

--- myfirst.js
+++ myfirst.js
@@ -1,12 +1,12 @@
 var http = require('http');
-var fs = require('fs');
 
 http.createServer(function (req, res) {
-    fs.readFile('demofile1.html', function (err, data) {
-        res.writeHead(200, {
-            'Content-Type': 'text/html'
-        });
-        res.write(data);
-        res.end();
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
     });
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
+    return res.end();
 }).listen(8080);
