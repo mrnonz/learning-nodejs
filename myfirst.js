30acee7002ac23c87a91482d638aaeb460fddc9d
Nontawat Numor <nontawat@jibsoft.co.th>
2019-08-12 14:34:21 +0700

    Add first app and module :tea:

---
+++ myfirst.js
@@ -0,0 +1,8 @@
+var http = require('http');
+var dt = require('./myfirstmodule');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write("The data and time are currently: " + dt.myDateTime());
+    res.end('Hello world');
+}).listen(8080);
